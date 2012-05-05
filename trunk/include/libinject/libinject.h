96a3323b1853c3debd53f8baa19dcb24d5225503
berserkovich@gmail.com <berserkovich@gmail.com@7b736c88-db85-6dcd-d2ae-937fe194f9f8>
2012-05-05 15:27:36 +0000

    added shared/static lib build option fixed some minor errors and warnings

--- trunk/include/libinject/libinject.h
+++ trunk/include/libinject/libinject.h
@@ -8,22 +8,36 @@ extern "C" {
 
 #ifdef _WIN32
 
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#   define WIN32_LEAN_AND_MEAN
+#   include <Windows.h>
 typedef HANDLE LIBINJECT_PROCESS;
 
 #else
-#error "Unknown platform"
+#   error "Unknown platform"
 #endif
 
+#ifdef LIBINJECT_SHARED
+#   ifdef _WIN32
+#       ifdef LIBINJECT_SHARED_BUILD
+#           define LBINJECT_EXPORT __declspec(dllexport)
+#       else
+#           define LIBNJECT_EXPORT __declspec(dllimport)
+#       endif
+#   endif
+#endif
+
+#ifndef LIBINJECT_EXPORT
+#   define LIBINJECT_EXPORT
+#endif
+    
 #define LIBINJECT_OK                0
 #define LIBINJECT_INVALID_PARAM     1
 #define LIBINJECT_ERROR             2
 //#define 
 
-int LIBINJECT_Inject( LIBINJECT_PROCESS _processHandle, const char* _libToInjectUtf8 );
+extern LIBINJECT_EXPORT int LIBINJECT_Inject( LIBINJECT_PROCESS _processHandle, const char* _libToInjectUtf8 );
 
-int LIBINJECT_StartInjected( const char* _commandlineUtf8, const char* _workingDirectoryUtf8, const char* const* _libsToInjectUtf8, LIBINJECT_PROCESS* _processHandle );
+extern LIBINJECT_EXPORT int LIBINJECT_StartInjected( const char* _commandlineUtf8, const char* _workingDirectoryUtf8, const char* const* _libsToInjectUtf8, LIBINJECT_PROCESS* _processHandle );
 
 
 #ifdef __cplusplus
